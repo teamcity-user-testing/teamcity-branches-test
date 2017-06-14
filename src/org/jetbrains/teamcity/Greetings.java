1a9dd27cf8b853989b2d7d3e2303ade3bc537522
shkate <shkate@jetbrains.com>
2017-06-14 08:39:30 +0200

    improve greetings

--- src/org/jetbrains/teamcity/Greetings.java
+++ src/org/jetbrains/teamcity/Greetings.java
@@ -4,6 +4,6 @@ package org.jetbrains.teamcity;
 public class Greetings {
 
     public static void main(String[] args) {
-        System.out.println("Greetings from TeamCity Team!");
+        System.out.println("Warm greetings from TeamCity Team!");
     }
 }
